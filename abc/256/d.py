5f3dddcdb125c6d6ce2f219d8376304309f9f560
bukubukutyagamaguti <bukubukutyagamaguti@gmail.com>
2022-06-19 23:18:36 +0900

    0619

--- abc/256/d.py
+++ abc/256/d.py
@@ -1,24 +1,34 @@
 import sys
 from typing import List
-import bisect 
 input = sys.stdin.readline
-limit = (10 ** 5)
-acc = [0] * limit
-acc[1] = 0
-for i in range(2, limit):
-    acc[i] = acc[i-1] + i
 
 def main():
-    # N = int(input())
-    # tmp = []
-    # for _ in range(N):
-    #     L, R = map(int, input().split())
-    #     tmp.append(abs(acc[R] - acc[L]))
-    tmp = [155, 255, 455]
-    index = bisect.bisect_left(acc, sum(tmp))
-    print(acc[index], sum(tmp), acc[index] - sum(tmp))
-    index2 = bisect.bisect_left(acc, acc[index] - sum(tmp))
-    print(acc[index2-1])
+    N = int(input())
+    li = []
+    for _ in range(N):
+        li.append(list(map(int,input().split())))
+
+    li = sorted(li, key = lambda x:x[0])
+    L = li[0][0]
+    R = li[0][1]
+    ans = []
+    index = 1
+    while True:
+        if index >= N:
+            ans.append([L, R])
+            break
+        if li[index][0] > R:
+            ans.append([L, R])
+            L = li[index][0]
+            R = li[index][1]
+            index += 1
+            continue
+        if li[index][1] > R:
+            R = li[index][1]
+
+        index += 1
+    for i in ans:
+        print(i[0], i[1])
     return
 
 main()
