4da1b3f63cc2ce3701793ecda8317664dd51dad9
bukubukutyagamaguti <bukubukutyagamaguti@gmail.com>
2022-07-06 10:13:26 +0900

    0706

---
+++ abc/203/a_test.py
@@ -0,0 +1,19 @@
+import sys
+import time
+from io import StringIO
+from typing import List
+# input = sys.stdin.readline
+# import pypyjit
+# pypyjit.set_param('max_unroll_recursion=-1')
+
+_INPUT = """\
+
+"""
+StartTime = time.time()
+sys.stdin = StringIO(_INPUT)
+
+def main():
+    return
+
+main()
+print(f'[Sec] {str(time.time() - StartTime)}')
