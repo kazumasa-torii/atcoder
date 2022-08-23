574ea756f89256e564fa48741455ce8515758843
bukubukutyagamaguti <bukubukutyagamaguti@gmail.com>
2022-08-23 10:46:00 +0900

    0823

--- agc/003/a.py
+++ agc/003/a.py
@@ -1,3 +1,16 @@
-import sys
-input = sys.stdin.readline
+def main():
+    s = set(list(input()))
+    flag = True
+    if 'N' in s:
+        if 'S' not in s:flag = False
+    if 'E' in s:
+        if 'W' not in s:flag = False
+    if 'W' in s:
+        if 'E' not in s:flag = False
+    if 'S' in s:
+        if 'N' not in s:flag = False
+    print('Yes' if flag else 'No')
+    return
 
+if __name__ == '__main__':
+    main()
--- agc/003/a_test.py
+++ agc/003/a_test.py
@@ -3,12 +3,24 @@ import time
 from io import StringIO
 
 _INPUT = """\
+NSNNSNSN
 
 """
 StartTime = time.time()
 sys.stdin = StringIO(_INPUT)
 
 def main():
+    s = set(list(input()))
+    flag = True
+    if 'N' in s:
+        if 'S' not in s:flag = False
+    if 'E' in s:
+        if 'W' not in s:flag = False
+    if 'W' in s:
+        if 'E' not in s:flag = False
+    if 'S' in s:
+        if 'N' not in s:flag = False
+    print('Yes' if flag else 'No')
     return
 
 if __name__ == '__main__':
