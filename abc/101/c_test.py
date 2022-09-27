3241c059e08095cf4a7a2b279e2cbdc415645536
bukubukutyagamaguti <bukubukutyagamaguti@gmail.com>
2022-09-27 10:31:54 +0900

    0927

--- abc/101/c_test.py
+++ abc/101/c_test.py
@@ -1,3 +1,4 @@
+
 """
 sortして愚直に全探索できるか考える
 その後に難しそうであれば下記アルゴリズムを考える
@@ -23,12 +24,25 @@ from io import StringIO
 from typing import List
 
 _INPUT = """\
+11 3
+7 3 1 8 4 6 2 5 6 3 4
 
 """
 StartTime = time.time()
 sys.stdin = StringIO(_INPUT)
 
 def main():
+    def judge(n, i):
+        i -= 1
+        cnt = 1
+        while n > 0:
+            n -= i
+            cnt += 1
+        return cnt
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    if n == k: exit(print(1))
+    print(judge(n-k, k))
     return
 
 if __name__ == '__main__':
